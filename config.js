09b59bfe8995e29d75de2136ffc23d6777ca0b0a
Alisha Byford <serenity4eternity786@gmail.com>
2016-12-17 03:20:27 +0000

    added mlab in config

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            'mongodb://serenity4eternity786:2loveany1@ds159507.mlab.com:59507/shopping-list');
 exports.PORT = process.env.PORT || 8080;
